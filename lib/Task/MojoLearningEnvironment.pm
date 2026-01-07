c0c844f25aae65e8660939585d2e385432042176
brian d foy <briandfoy@pobox.com>
2026-01-07 10:19:53 -0500

    Updated copyright year

--- lib/Task/MojoLearningEnvironment.pm
+++ lib/Task/MojoLearningEnvironment.pm
@@ -41,7 +41,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2015-2021, brian d foy <briandfoy@pobox.com>. All rights reserved.
+Copyright © 2015-2026, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
